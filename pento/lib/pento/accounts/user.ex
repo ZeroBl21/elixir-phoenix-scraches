876ba16e9cc4f7348bdef2ed2cfc57c5b80a76eb
Darwin Perdomo <ZeroBl@hotmail.com>
2024-10-17 16:36:07 -0400

    feat(pento/): Modify user registration and validation

--- pento/lib/pento/accounts/user.ex
+++ pento/lib/pento/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule Pento.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
@@ -37,9 +38,17 @@ defmodule Pento.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :username])
     |> validate_email(opts)
+    |> validate_username(opts)
     |> validate_password(opts)
+  end
+
+  defp validate_username(changeset, opts) do
+    changeset
+    |> validate_required([:username])
+    |> validate_length(:username, max: 160)
+    |> maybe_validate_unique_username(opts)
   end
 
   defp validate_email(changeset, opts) do
@@ -83,6 +92,16 @@ defmodule Pento.Accounts.User do
       changeset
       |> unsafe_validate_unique(:email, Pento.Repo)
       |> unique_constraint(:email)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_validate_unique_username(changeset, opts) do
+    if Keyword.get(opts, :validate_username, true) do
+      changeset
+      |> unsafe_validate_unique(:username, Pento.Repo)
+      |> unique_constraint(:username)
     else
       changeset
     end
