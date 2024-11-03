ac27993520dfe0bac962b1bd4419bb6ef3ef3827
Darwin Perdomo <ZeroBl@hotmail.com>
2024-11-03 15:24:15 -0400

    fix(pento/promo.ex): Fix typos

--- pento/lib/pento/promo.ex
+++ pento/lib/pento/promo.ex
@@ -1,12 +1,12 @@
 defmodule Pento.Promo do
   alias Pento.Promo.Recipient
 
-  def change_recipent(%Recipient{} = recipent, attrs \\ %{}) do
-    Recipient.changeset(recipent, attrs)
+  def change_recipient(%Recipient{} = recipient, attrs \\ %{}) do
+    Recipient.changeset(recipient, attrs)
   end
 
-  def send_promo(_recipent, _attrs) do
-    # Programmer 1: Insert logic to email to promo recipent
+  def send_promo(_recipient, _attrs) do
+    # Programmer 1: Insert logic to email to promo recipient
     # Programmer 2: No.
     {:ok, %Recipient{}}
   end
